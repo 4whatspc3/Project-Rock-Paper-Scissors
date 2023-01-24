09afd344c1275747f701fd1f321a3e81874e19b4
Matheus Augusto <mathieus_n.7@hotmail.com>
2023-01-24 15:51:08 -0300

    Create a new function to play a single round.

--- script.js
+++ script.js
@@ -15,5 +15,26 @@ function getComputerChoice() {
 }
 
 let computerSelection = getComputerChoice();
+let playerSelection = 'rock';
 
-console.log(computerSelection)
+function playRound(computerSelection, playerSelection){
+    // rename parameters to make comparisons easier
+    const c = computerSelection,
+          p = playerSelection;
+    // a new variable to store the result
+    let result;
+
+    if (c === p) {
+        result = `Draw`;
+    } else if (c == 'rock' && p == 'scissors' || 
+               c == 'paper' && p == 'rock' || 
+               c == 'scissors' && p == 'paper') {
+                    result = `You Lose! ${c} beats ${p}`;
+    } else {
+        result = `You Won! ${p} beats ${c}`;
+    }
+
+    return result;
+}
+
+console.log(playRound(computerSelection, playerSelection))
